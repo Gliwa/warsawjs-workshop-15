d53c24959543c65a7fd9486603c09e57e0d8f078
Mike <contact@devmike.eu>
2018-01-07 12:37:16 +0100

    Step 4
    1) declare playerClasses object to hold color for each player (playerA - 'red' and playerB - 'blue')
    2) declare currentPlayer variable to hold currentPlayer name
    3) Remove Click from handler function

--- game.js
+++ game.js
@@ -1,15 +1,37 @@
+// STEP 3
+var playerClasses = {
+  'playerA': 'red',
+  'playerB': 'blue'
+};
+
+var currentPlayer = 'playerA';
+
 document.addEventListener('DOMContentLoaded', function() {
 
   initGame();
 
   function initGame() {
+
+    // STEP 1
     var fields = document.querySelectorAll('.board > div');
     fields.forEach(field => field.addEventListener('click', fieldClickHandler));
-  }
+  };
 
   function fieldClickHandler() {
+    // declare playerClasses object to hold color for each player (playerA - 'red' and playerB - 'blue')
+    // declare currentPlayer variable to hold currentPlayer name
+    var playerClass = playerClasses[currentPlayer];
 
-    this.classList.add('red');
-  }
+    this.classList.add(playerClass);
 
+    // assign to it name of the color of currentPlayer
+    if (currentPlayer === 'playerA') {
+      currentPlayer = 'playerB';
+    } else {
+      currentPlayer = 'playerA';
+    };
+
+    // Remove Click from handler function
+    this.removeEventListener('click', fieldClickHandler);
+  };
 });
