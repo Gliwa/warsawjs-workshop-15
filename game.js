9b411fbb9a0a552e1e9b82a1037bd4f7d94eafb8
Mike <contact@devmike.eu>
2018-01-07 14:03:58 +0100

    Step 6
    Create new checkWinner

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-// STEP 3
+// Create variable playerClasses
 var playerClasses = {
   'playerA': 'red',
   'playerB': 'blue'
@@ -6,7 +6,7 @@ var playerClasses = {
 
 var currentPlayer = 'playerA';
 
-// create variable in which you will store number of empty fileds left
+// Create variable in which you will store number of empty fileds left
 var emptyFields;
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -21,8 +21,8 @@ document.addEventListener('DOMContentLoaded', function() {
   };
 
   function fieldClickHandler() {
-    // declare playerClasses object to hold color for each player (playerA - 'red' and playerB - 'blue')
-    // declare currentPlayer variable to hold currentPlayer name
+    // Declare playerClasses object to hold color for each player (playerA - 'red' and playerB - 'blue')
+    // Declare currentPlayer variable to hold currentPlayer name
     var playerClass = playerClasses[currentPlayer];
 
     this.classList.add(playerClass);
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Decrese empty fields by 1
     emptyFields--;
 
-    // assign to it name of the color of currentPlayer
+    // aAsign to it name of the color of currentPlayer
     if (currentPlayer === 'playerA') {
       currentPlayer = 'playerB';
     } else {
@@ -40,9 +40,57 @@ document.addEventListener('DOMContentLoaded', function() {
     // Remove Click from handler function
     this.removeEventListener('click', fieldClickHandler);
 
+    checkWinner();
     // Alert
     if (emptyFields === 0) {
       alert('KONIEC GRY');
-    }
+    };
+
+    function checkWinner() {
+      fields = document.querySelectorAll('.board > div');
+
+      // Create string for each winning configuration 012, 345, 789
+      var row1 = fields[0].className + fields[1].className + fields[2].className;
+      var row2 = fields[3].className + fields[4].className + fields[5].className;
+      var row3 = fields[6].className + fields[7].className + fields[8].className;
+
+      // Create string for each winning configuration 036, 147, 258
+      var column1 = fields[0].className + fields[3].className + fields[6].className;
+      var column2 = fields[1].className + fields[4].className + fields[7].className;
+      var column3 = fields[2].className + fields[5].className + fields[8].className;
+
+      // Create string for each winning configuration 048, 642
+      var diagonal1 = fields[0].className + fields[4].className + fields[8].className;
+  		var diagonal2 = fields[6].className + fields[4].className + fields[2].className;
+
+
+      if (row1 === 'redredred' ||
+          row2 === 'redredred' ||
+          row3 === 'redredred' ||
+          column1 === 'redredred' ||
+          column2 === 'redredred' ||
+          column3 === 'redredred' ||
+          diagonal1 === 'redredred' ||
+          diagonal2 === 'redredred'
+        ) {
+        alert('Red Win');
+        return;
+      };
+
+      if (row1 === 'blueblueblue' ||
+          row2 === 'blueblueblue' ||
+          row3 === 'blueblueblue' ||
+          column1 === 'blueblueblue' ||
+          column2 === 'blueblueblue' ||
+          column3 === 'blueblueblue' ||
+          diagonal1 === 'blueblueblue' ||
+          diagonal2 === 'blueblueblue'
+        ) {
+        alert('Blue Win');
+        return;
+      };
+
+    };
   };
+
 });
