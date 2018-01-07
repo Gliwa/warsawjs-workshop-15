a4171715d5c7442f165b7b1ae3cd0b5ef067cf38
Mike <contact@devmike.eu>
2018-01-07 11:06:36 +0100

    Step 1

---
+++ game.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  initGame();
+
+  function initGame() {
+    var fields = document.querySelectorAll('.board > div');
+    fields.forEach(field => field.addEventListener('click', fieldClickHandler));
+  }
+
+  function fieldClickHandler() {
+    console.log("Hello", this);
+  }
+});
