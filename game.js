ae99cda07eff06f10ce634a3a1ce5f5ba5d0949d
Mike <contact@devmike.eu>
2018-01-07 14:19:39 +0100

    Step 6a
    Make checks

--- game.js
+++ game.js
@@ -55,38 +55,31 @@ document.addEventListener('DOMContentLoaded', function() {
       var row3 = fields[6].className + fields[7].className + fields[8].className;
 
       // Create string for each winning configuration 036, 147, 258
-      var column1 = fields[0].className + fields[3].className + fields[6].className;
-      var column2 = fields[1].className + fields[4].className + fields[7].className;
-      var column3 = fields[2].className + fields[5].className + fields[8].className;
+      var col1 = fields[0].className + fields[3].className + fields[6].className;
+      var col2 = fields[1].className + fields[4].className + fields[7].className;
+      var col3 = fields[2].className + fields[5].className + fields[8].className;
 
       // Create string for each winning configuration 048, 642
-      var diagonal1 = fields[0].className + fields[4].className + fields[8].className;
-  		var diagonal2 = fields[6].className + fields[4].className + fields[2].className;
+      var diag1 = fields[0].className + fields[4].className + fields[8].className;
+      var diag2 = fields[6].className + fields[4].className + fields[2].className;
 
+      // Make checks
+      var winning = [
+        row1,
+        row2,
+        row3,
+        col1,
+        col2,
+        col3,
+        diag1,
+        diag2
+      ];
 
-      if (row1 === 'redredred' ||
-          row2 === 'redredred' ||
-          row3 === 'redredred' ||
-          column1 === 'redredred' ||
-          column2 === 'redredred' ||
-          column3 === 'redredred' ||
-          diagonal1 === 'redredred' ||
-          diagonal2 === 'redredred'
-        ) {
-        alert('Red Win');
+      if (winning.includes('redredred')) {
+        alert("Red Wins!");
         return;
-      };
-
-      if (row1 === 'blueblueblue' ||
-          row2 === 'blueblueblue' ||
-          row3 === 'blueblueblue' ||
-          column1 === 'blueblueblue' ||
-          column2 === 'blueblueblue' ||
-          column3 === 'blueblueblue' ||
-          diagonal1 === 'blueblueblue' ||
-          diagonal2 === 'blueblueblue'
-        ) {
-        alert('Blue Win');
+      } else if (winning.includes('blueblueblue')) {
+        alert("Blue Wins!");
         return;
       };
 
